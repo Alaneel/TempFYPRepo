ac59b736672edc989396de54e6745c81fb96ecd6
Alaneel <alanwang@Apples-MacBook-Pro.local>
2026-01-04 22:37:46 +0800

    Update

--- run_details_only.py
+++ run_details_only.py
@@ -15,8 +15,8 @@ def main():
     try:
         # 配置
         config = {
-            'apikey': 'c739d557371a40bab543b2957f668b68',  # 填入你的API密钥
-            'proxy': '90601315-res_snjf7k2ban7:ikgcradf@gw-res.cloudbypass.com:1288'
+            'apikey': '',  # 填入你的API密钥
+            'proxy': ''
         }
 
         # 创建Pipeline实例（使用15个线程加快处理）
--- run_full.py
+++ run_full.py
@@ -22,8 +22,8 @@ def main():
     try:
         # 配置
         config = {
-            'apikey': 'c739d557371a40bab543b2957f668b68',  # 填入你的API密钥
-            'proxy': '90601315-res_sx5prizpxmi:ikgcradf@gw-res.cloudbypass.com:1288'
+            'apikey': '',  # 填入你的API密钥
+            'proxy': ''
         }
 
         # 创建Pipeline实例（使用10个线程）
